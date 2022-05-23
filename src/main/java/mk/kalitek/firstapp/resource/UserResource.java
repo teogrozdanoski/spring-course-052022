f8246770018b236653abbb5c94cdaa561cb286f7
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-05-23 17:21:07 +0200

    First commit of project

---
+++ src/main/java/mk/kalitek/firstapp/resource/UserResource.java
@@ -0,0 +1,15 @@
+package mk.kalitek.firstapp.resource;
+
+import mk.kalitek.firstapp.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/user")
+public class UserResource {
+    @GetMapping("/home")
+    public String showUser(){
+        return "Application works";
+    }
+}
